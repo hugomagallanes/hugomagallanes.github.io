5d17a1149ff4482f7628710b09b8473d5971b9a3
Hugo Magallanes <hugopdmagalhaes@gmail.com>
2022-01-17 16:15:19 +0100

    Added Super folder
    Added Super folder containing the CSS and JS files for my Notion portfolio

---
+++ Super/site-min.js
@@ -0,0 +1,6 @@
+$(document).ready((function(){$('.super-navbar__item-list a[href*="https://gumroad.com/l/puthJh"]').each((function(){$(".super-navbar__item-list .super-navbar__item").last().addClass("buy-now")}))}));
+Viewport
+Window
+×
+×
+×
